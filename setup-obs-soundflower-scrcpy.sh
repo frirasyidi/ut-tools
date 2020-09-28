7a3a7df90a116ad6a97202499c13a66133fbfc38
Fri Rasyidi <fri@frirasyidi.com>
2020-09-28 11:30:10 +0700

    Adding the shell file

---
+++ setup-obs-soundflower-scrcpy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install scrcpy
+brew cask install android-platform-tools
+brew cask install obs
+brew cask install soundflower
